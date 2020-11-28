dadcb91f1ce1a469091a68f2639fd1f8d691a8d0
eduardo.ramirez <eduardo.ramirez@crossmatch.com>
2020-11-28 14:53:18 -0500

    ER - Finished with Square pair random assignment of values

--- gameConcentrese/src/MatrixBox.js
+++ gameConcentrese/src/MatrixBox.js
@@ -10,16 +10,37 @@ function MatrixBox( props )  {
 
 function MatrixGroup( props ) {
     let boxArray = [];
-    for( let counter=1; counter<=Number( props.numBox ); counter++ ) {
-        //boxArray.push( counter.toString() );
-        boxArray.push( Math.floor( Math.random() * ( Math.floor( 1002 ) - Math.ceil( 1 ) + 1 ) ) );  
+    let boxObj   = {};
+    let minVal=0;
+    let maxVal=0;
+    // Loop to assign random values per pair of squares
+    for( let counter=1; counter<=( Number( props.numBox ) ); counter++ ) {
+        if( boxObj[counter] === undefined ) {
+            // Assign random value to current square
+            minVal=Math.ceil( 1 );
+            maxVal=Math.floor( 1002 );
+            boxObj[counter]=Math.floor( Math.random() * ( maxVal - minVal + 1 ) + minVal );
+            // Assign ame value to random Square pair
+            minVal=Math.ceil( counter );
+            maxVal=Math.floor( Number( props.numBox ) );
+            var selBox=Math.floor( Math.random() * ( maxVal - minVal + 1 ) + minVal );
+            while( boxObj[selBox] !== undefined ) {
+                selBox=Math.floor( Math.random() * ( maxVal - minVal + 1 ) + minVal );
+            }
+            boxObj[selBox]=boxObj[counter];
+        }
     }
-//console.log( retObject )
+    // Loop to set array of values chosen
+    for( let item in boxObj ){
+        boxArray.push( boxObj[item] );
+    }
+    console.log(boxObj);
+
     return (
         <div  className='MatrixBox'>
             <h1 className='Title'>CONCENTRESE</h1>
             {boxArray.map(( boxId, index ) => (
-                <MatrixBox key={index + 1} value={index + 1}>{boxId}</MatrixBox>
+                <MatrixBox key={index + 1} value={boxId}>{boxId}</MatrixBox>
             ))}
         </div>
     )
